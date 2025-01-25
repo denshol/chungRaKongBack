be22f8c8a0f5de2235a5b260b9c477bfb14fa677
KimJunHyeok <denshol0709@gmail.com>
2025-01-25 16:22:14 +0900

    ds

--- server.js
+++ server.js
@@ -36,7 +36,6 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  console.log(process.env.MONGODB_URI);
 });
